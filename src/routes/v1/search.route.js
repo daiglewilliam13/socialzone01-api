559b1a0a81e254b80c8921ba587de2212f10389d
William Daigle <daiglewilliam13@gmail.com>
2022-06-27 11:12:04 -0700

    add db callbacks

--- src/routes/v1/search.route.js
+++ src/routes/v1/search.route.js
@@ -5,12 +5,15 @@ const authController = require('../../controllers/auth.controller');
 const auth = require('../../middlewares/auth');
 const router = express.Router();
 const mongoose = require('mongoose');
-
+const Post = require('../../models/post.model')
+const User = require('../../models/user.model');
 
 router
     .route('/')
-    .get((req, res) => {
-        res.send('search get route')
+    .get(async(req, res) => {
+        let posts = await Post.find({}).then((data)=>{return data})
+        let users = await User.find({}).then((data)=>{return data})
+        res.send({posts, users})
     })
 
 module.exports = router;
