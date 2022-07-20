14c974d334fece7e9b5e5f900a225c61d305c5c7
William Daigle <daiglewilliam13@gmail.com>
2022-07-19 23:07:08 -0700

    build engagement model

--- src/models/engagement.model.js
+++ src/models/engagement.model.js
@@ -2,7 +2,23 @@ const mongoose = require('mongoose');
 
 
 const engagementSchema = mongoose.Schema({
-
+    _id: {
+        type: mongoose.types.ObjectId;
+    },
+    authorId: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+    },
+    replyTo: {
+        type: mongoose.Types.ObjectId,
+        ref: "Post",
+    },
+    replyType: {
+        type: string,
+    },
+    createdAt: {
+        type:Date
+    }
 })
 
 const Engagement = mongoose.model('Engagement', engagementSchema);
