34e9f9e36b4245b10df7f5681bf0617b4683f69d
William Daigle <daiglewilliam13@gmail.com>
2022-07-27 16:20:42 -0700

    build message post route object build/save

--- src/routes/v1/messages.route.js
+++ src/routes/v1/messages.route.js
@@ -5,12 +5,20 @@ const authController = require('../../controllers/auth.controller');
 const auth = require('../../middlewares/auth');
 const mongoose = require('mongoose');
 const router = express.Router();
-const Notification = require('../../models/notification.model')
+const Message = require('../../models/message.model')
 
 router
     .route('/')
     .get(async (req, res) => {
         res.send('messages route')
+    })
+    .post(async (req,res)=>{
+        console.log(req.body);
+        const id = new mongoose.Types.ObjectId();
+        const obj = { _id: id, ...req.body };
+        const newMessage = new Message(obj);
+        const result = await newMessage.save();
+        res.send(result);
     })
 
 module.exports = router;
